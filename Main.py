bdb1d066f6edd289770e685403f383d82d2b9d3a
Decadanza <banunajieh@gmail.com>
2026-01-11 10:30:46 +0700

    Menu

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 import ListBarang
+import os
 
 class Menu:
     def __init__(self):
         self.menu_func = [
             self.TambahData,
-            self.RejectBarang,
+            self.CekBarang,
             self.SearchBarang,
             self.AddPesananCustumer,
             self.CekPackingBarang
@@ -12,9 +13,11 @@ class Menu:
 
     def TambahData(self): # Bintang --
         print("Tambah Data")
+        # Tesseract.AddBarangBaru(101, "Aqua Botol 250ml", 20)
 
-    def RejectBarang(self): # Bintang --
+    def CekBarang(self): 
         print("Reject Barang")
+        # Tesseract.TampilData()
 
     def SearchBarang(self): # Albani----
         print("Search Barang")
@@ -23,34 +26,33 @@ class Menu:
         print("Add Pesanan Customer")
 
     def CekPackingBarang(self): # Michdan---
-        print("Add Pesanan Customer")   
+        print("Add Pesanan Customer")
 
     def run(self, i):
-        if 1 <= i <= len(self.menu_func):
-            self.menu_func[i]
+        if 0 <= i < len(self.menu_func):
+            self.menu_func[i]()
             return
         else:
-            print("Menu Tidak Tersedia!!")
+            print("Menu Tidak Tersedia!")
             return
 
+Tesseract = ListBarang.ListBarang("Werehouse.comp")
 
 if __name__ == "__main__":
-    Tesseract = ListBarang.ListBarang("X.Comp")
     menu = Menu();
     while(True):
+        os.system('cls')
         print(f"========== Management Gudang ==========")
         print("Menu Gudang: ")
         print("1. Add Barang Baru. ")
-        print("2. Reject Barang. ")
+        print("2. Cek Barang. ")
         print("3. Cari barang. ")
         print("4. Add Pesanan Barang. ")
         print("5. Cek Packing Barang. ")
         print("0. Keluar. ")
         choice = int(input("Pilih Menu: "))
-        if(choice == 0):
+        if(choice <= 0):
+            print("---- Keluar APP.....")
             break
-        menu.run(choice)
-        
-    Tesseract.AddBarangBaru(101, "Aqua Botol 250ml", 20)
-
-    Tesseract.TampilData()
+        menu.run(choice-1)
+        os.system('pause')
